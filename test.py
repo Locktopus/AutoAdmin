fc8c74c48ef386806c0e263d042d574c04d5744f
Locktopus <florianlennuyeux@gmail.com>
2020-01-17 18:15:07 +0100

    starter

--- test.py
+++ test.py
@@ -1,12 +1,1 @@
-<<<<<<< HEAD
-print("hello world")
-
-"""
-A quoi sert la fonction.
-"""
-
-def funcname(self, parameter_list):
-    pass
-=======
 print("hello world\n")
->>>>>>> 6cf3b91efae3840aaab41514693aa1a528fdb419
